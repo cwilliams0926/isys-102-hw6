607f51a15d8a7e7d26ccde7aa2a0e44caf3bfc17
cwilliams0926 <coopwilliams03@hotmail.com>
2025-04-15 10:30:48 -0400

    Add pseudocode for script

---
+++ script.js
@@ -0,0 +1,21 @@
+const processButton = document.querySelector("#process");
+processButton.addEventListener("click", makeStringArray); //getInput is placeholder
+
+function makeStringArray() {
+    const input = document.querySelector("#sentence").value;
+    // trim input string
+    // filter array for numbers
+    // filter array for words
+    // pass number array to 
+}
+
+function listNumbers() {
+    // iterate through number array
+    // append each element to number-words <ul>
+}
+
+function listWords() {
+    // iterate through word array
+    // append each element to non-number-words <ul>
+}
+
